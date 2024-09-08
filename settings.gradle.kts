760d227b0bc0873e1d33770177a1b91c70b867e5
Naveen Singh <snaveen935@gmail.com>
2024-09-09 00:26:34 +0530

    Remove `includeBuild()` comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,10 +19,3 @@ dependencyResolutionManagement {
 rootProject.name = "Gallery"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
-
-// TODO: This will be deprecated in future. Migrate to the newer `pluginManagement { includeBuild() }` mechanism instead of explicitly substituting dependency.
-/*includeBuild("../Commons") {
-    dependencySubstitution {
-        substitute(module("org.fossify:commons")).using(project(":commons"))
-    }
-}*/
