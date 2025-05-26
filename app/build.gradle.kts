69f09373c3f912e73293786c8d230b8ecc4c6d95
Naveen Singh <36371707+naveensingh@users.noreply.github.com>
2025-05-26 14:14:48 +0530

    build: move app version to gradle.properties (#485)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,11 @@ android {
     compileSdk = project.libs.versions.app.build.compileSDKVersion.get().toInt()
 
     defaultConfig {
-        applicationId = libs.versions.app.version.appId.get()
+        applicationId = project.property("APP_ID").toString()
         minSdk = project.libs.versions.app.build.minimumSDK.get().toInt()
         targetSdk = project.libs.versions.app.build.targetSDK.get().toInt()
-        versionName = project.libs.versions.app.version.versionName.get()
-        versionCode = project.libs.versions.app.version.versionCode.get().toInt()
+        versionName = project.property("VERSION_NAME").toString()
+        versionCode = project.property("VERSION_CODE").toString().toInt()
         setProperty("archivesBaseName", "gallery-$versionCode")
     }
 
@@ -101,7 +101,7 @@ android {
         kotlinOptions.jvmTarget = project.libs.versions.app.build.kotlinJVMTarget.get()
     }
 
-    namespace = libs.versions.app.version.appId.get()
+    namespace = project.property("APP_ID").toString()
 
     lint {
         checkReleaseBuilds = false
