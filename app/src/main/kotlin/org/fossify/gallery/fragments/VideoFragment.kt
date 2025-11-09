ab704edaa5198928dec13216868a0846ae49b3d5
Naveen Singh <snaveen935@gmail.com>
2025-11-09 14:46:20 +0530

    feat: disable auto-play on seek
    This keeps the behavior consistent with Music Player (see https://github.com/FossifyOrg/Music-Player/issues/97)

--- app/src/main/kotlin/org/fossify/gallery/fragments/VideoFragment.kt
+++ app/src/main/kotlin/org/fossify/gallery/fragments/VideoFragment.kt
@@ -766,8 +766,6 @@ class VideoFragment : ViewPagerFragment(), TextureView.SurfaceTextureListener,
 
         if (mIsPlaying) {
             mExoPlayer!!.playWhenReady = true
-        } else {
-            playVideo()
         }
 
         mIsDragged = false
