2e3dbf8a801b712334c9bc6ddbcc916d268d7ffe
Neer-rn <neerrnbhatta@gmail.com>
2025-12-28 21:22:09 +0545

    Fix video resuming playback while seeking if paused

--- app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
+++ app/src/main/kotlin/org/fossify/gallery/activities/VideoPlayerActivity.kt
@@ -783,7 +783,9 @@ open class VideoPlayerActivity : BaseViewerActivity(), SeekBar.OnSeekBarChangeLi
         mExoPlayer?.playWhenReady = false
         mPlayWhenReadyHandler.removeCallbacksAndMessages(null)
         mPlayWhenReadyHandler.postDelayed({
-            mExoPlayer?.playWhenReady = true
+            if (mIsPlaying) {
+                mExoPlayer?.playWhenReady = true
+            }
         }, PLAY_WHEN_READY_DRAG_DELAY)
     }
 
